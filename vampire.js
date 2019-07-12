af159b23a0020a233ebe86f71e917d0fcf6150d7
Simon Garber <Simon.garber@gmail.com>
2019-07-12 18:53:29 -0400

    finished each of the sections and passed all of the tests

--- vampire.js
+++ vampire.js
@@ -10,24 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    this.offspring.creator = this;
+    vampire.creator = this;
   }
-
+  
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    
+    if (this.numberOfVampiresFromOriginal === 0 || this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+    } return false;
   }
-
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -36,9 +47,37 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let juniorVampire;
+    let seniorVampire;
+    if (!this.creator) {
+      return this;
+    } else if (this === vampire) {
+      return vampire;
+    } else
 
+    if (this.isMoreSeniorThan(vampire)) {
+      seniorVampire = this;
+      juniorVampire = vampire;
+      
+    } else {
+      seniorVampire = vampire;
+      juniorVampire = this;
+    }
+    let currentVampire = juniorVampire;
+    while (currentVampire.creator) {
+      if (currentVampire.creator.offspring.indexOf(seniorVampire) !== -1 || currentVampire.creator === seniorVampire) {
+        return currentVampire.creator;
+      } currentVampire = currentVampire.creator;
+    }
+    if (!currentVampire.creator) {
+    } return currentVampire;
   }
 }
+
+
+
+    
+
 
 module.exports = Vampire;
 
